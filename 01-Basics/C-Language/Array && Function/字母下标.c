00e893559fb149ed158fdfb8a3c7ff312a169d0f
Xeayy <2533019789@qq.com>
2026-03-07 20:23:25 +0800

    .

---
+++ 01-Basics/C-Language/Array && Function/字母下标.c
@@ -0,0 +1,18 @@
+/*#include <stdio.h>
+
+int main(void){
+
+    printf("%d",'A');
+
+    return 0;
+
+}    */
+                //得出A = 65
+
+int main(void){
+    int a[255];
+    a['A'] = 1;
+
+    printf("%d",a[65]);
+    return 0;
+}
--- 01-Basics/C-Language/Others/最大公约数.c
+++ 01-Basics/C-Language/Others/最大公约数.c
@@ -10,7 +10,7 @@ int main(void){
         a = b;
         b = t;
     } 
-    printf("gcd = %d\n",a);
+    printf("GCD = %d\n",a);
 
     return 0;
 }
