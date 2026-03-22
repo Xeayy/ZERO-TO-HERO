92b4877bdecc06401fe01d1e3fbcddf6d1872c40
Xeayy <2533019789@qq.com>
2026-03-22 23:22:02 +0800

    3.22

--- 01-Basics/Python/字面量.py
+++ 01-Basics/Python/字面量.py
@@ -1,10 +1,21 @@
 """我是文档字符串"""
 
 '张三'
-18
+a = 18
 
 '李四'
-114514
+b = 114514
 
-print('李四', 114514, '李四李四李')
-print('滚木滚木滚木', 114514)
+print('李四', b, '李四李四李')
+print('滚木滚木滚木', b)
+
+print(100) # 整数(int)
+print(3.14) # 浮点数/小数(float)
+print(True) # 布尔(bool)
+print('Hello Python') # 字符串(str)
+print('------------------') # 字符串(str)
+print(None) # 空值(NoneType)
+
+# 布尔类型本质也是整数类型(True - 1; False - 0)
+print(True + 1) # 2
+print(False - 1) # -1
