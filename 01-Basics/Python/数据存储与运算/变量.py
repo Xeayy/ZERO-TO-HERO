de38aa355a202c8a2f3801c5eb54e827f2de6bfc
Xeayy <2533019789@qq.com>
2026-03-30 21:59:51 +0800

    3.30

---
+++ 01-Basics/Python/数据存储与运算/变量.py
@@ -0,0 +1,16 @@
+# 变量 --->Python是动态类型语言，一个变量是可以存储不同类型的数据的
+#（但是项目开发中，推荐变量只存储一种类型的数据）
+num = 114514
+print(num)
+
+num = num + 1919810
+print(num)
+
+num = 'OK'
+print(num)
+
+num = True
+print(num)
+
+a = True
+print(a)
