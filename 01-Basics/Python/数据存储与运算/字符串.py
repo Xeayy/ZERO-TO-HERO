ff8492d1b00b0e50f0fde429a0130faea80d3074
Xeayy <2533019789@qq.com>
2026-03-31 23:41:31 +0800

    3.31

---
+++ 01-Basics/Python/数据存储与运算/字符串.py
@@ -0,0 +1,29 @@
+#双引号定义
+s1 = "Hello"
+
+#单引号定义
+s2 = 'Python'
+
+#换个行试试
+s3 = '1145' \
+'14'
+
+#三引号定义
+s4 = """
+        我能
+        支持
+        换行
+"""
+print(s1,s2,s3,s4,sep='\n')
+
+print(type(s1))
+print(type(s2))
+print(type(s3))
+print(type(s4))
+
+msg = 'It\'s interesting!'
+msg2 = "It's interesting!"
+print(msg)
+print(msg2)
+
+print("\t说的道理\n\t说的道理")
