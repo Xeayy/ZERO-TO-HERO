6ccb56245924a297bb08ab283fdddd149027e9e2
Xeayy <2533019789@qq.com>
2026-04-04 19:42:22 +0800

    4.4

---
+++ 01-Basics/Python/数据存储与运算/字符串拼接.py
@@ -0,0 +1,20 @@
+slogan = '原神' '启动' # 多个字符串字面量直接写
+print(slogan)
+
+slogan = '原神' + '牛逼' # +号码拼接
+print(slogan)
+
+s1 = '一点'
+s2 = '也不牛逼'
+
+print('原神' + ','+ s1  + s2)
+
+
+
+name = 'Xander'
+age = 18
+major = '信息安全'
+hobby = 'AI'
+
+print('大家好，我是' + name + '，今年' + str(age) + '岁，学习的专业是' + major + '，爱好是' + hobby)
+print(f'大家好，我是{name}，今年{age}岁，学习的专业是{major}，爱好是{hobby}')
