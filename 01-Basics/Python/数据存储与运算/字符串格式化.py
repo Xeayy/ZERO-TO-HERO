6d76b50d4a1b23e06a959a36dbb1f7350cca7942
Xeayy <2533019789@qq.com>
2026-04-05 18:10:54 +0800

    4.5

---
+++ 01-Basics/Python/数据存储与运算/字符串格式化.py
@@ -0,0 +1,11 @@
+s1 = '人生苦短'
+s2 = '我用Python'
+
+print('11%s,%s' % (s1,s2))
+
+name = 'Xander'
+age = 18
+major = '信息安全'
+hobby = 'AI'
+
+print('大家好，我是%s ，今年%s岁，学习的专业是%s ，爱好是%s' % (name, age, major, hobby))
