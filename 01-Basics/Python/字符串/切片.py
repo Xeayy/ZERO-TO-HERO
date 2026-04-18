00fa4b5971ee80be11ab5be00dceab83d76f535c
Xeayy <2533019789@qq.com>
2026-04-18 23:27:36 +0800

    4.18

---
+++ 01-Basics/Python/字符串/切片.py
@@ -0,0 +1,24 @@
+a = 'My name is Xander'
+
+c = a[:]
+print(c)
+
+b = a[:11]
+print (b)
+
+a = a[11:17]
+print (a)
+
+name = 'My name is Xander'
+name = name.replace('Xander','Xeay')
+print(name)
+
+arr = name.split(' ')
+print(arr)
+
+string = '-'.join(arr)
+print (string)
+
+string1 = 'Hello'
+s = string1[1]
+print (s)
---
+++ 01-Basics/Python/流程控制语句/死循环.py
@@ -0,0 +1,4 @@
+while True:
+    for i in range(10):
+        print('我错了')
+    print('下次还敢')
